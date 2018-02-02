db2707154c9252bd340e052f8e97c60b63cb05e7
eczn <1975845422@qq.com>
2018-02-03 01:55:18 +0800

    [M] parse 模块分割

--- src/parse/index.js
+++ src/parse/index.js
@@ -1,4 +1,4 @@
-const { S, $ } = require('./S');
+const getExp = require('./getExp')
 
 module.exports = parse; 
 
@@ -30,49 +30,4 @@ function parse(text){
     exp.log(); 
 
     return exp; 
-}
-
-function getExp(chars){
-    let bracket = 0, args = [];
-
-    for (let i = 0; i < chars.length; i++){
-        let char = chars[i]; 
-
-        if (char === '('){
-            bracket = bracket + 1; 
-            if (bracket === 1){
-                // push next 
-                args.push(chars[i + 1]);
-                i = i + 1; 
-
-            } else if (bracket === 2) {
-                let innerExp = chars.slice(i); 
-                let { exp, len } = getExp(innerExp); 
-
-                args.push(exp); 
-
-                i = i + len - 1; 
-            }
-        } else if (char === ')'){
-            bracket = bracket - 1; 
-            if (bracket === 0){
-                // 回归 
-                return {
-                    exp: $(args), 
-                    len: i
-                } 
-            } else {
-                // do nothing ...
-            }
-        } else {
-            if (bracket === 1){
-                args.push(char);                 
-            }
-        }
-    }
-
-    return {
-        exp: $(args), 
-        len: i
-    } 
 }
