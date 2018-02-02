33e594848ec91a83fe61f3951e107372f418760f
bozhi.lai <bozhi.lai@vipshop.com>
2018-02-02 17:11:22 +0800

    [A] 添加必要的注释

--- S.js
+++ S.js
@@ -1,38 +1,48 @@
 module.exports = S; 
 
+// DEBUG MODE 
+S.DEBUG = false; 
+
+// Helper Func 
+/**
+ * @description 返回定长的 tab 
+ * @param   { Number }  deep 
+ * @returns { String }
+ */
+let tab = deep => '    '.repeat(deep); 
+
 /**
  * @description 构造 S 表达式
- * @param { Array }  list
+ * @param { Array } list 
  */
 function S(list){
     this.list = list;
 }
 
-S.DEBUG = false; 
+/**
+ * @description Alias 
+ * @param   { String } todo 
+ * @returns { S } 返回 S 表达式 
+ */
+S.$ = list => new S(list); 
 
-let tab = deep => '    '.repeat(deep); 
-
+/**
+ * @description S 表达式自己打印自己 
+ * @param { Number  } deep = 0, 深度 
+ */
 S.prototype.log = function(deep = 0){
     let [ todo, ...args ] = this.list; 
 
     let before = s => ((s ? 's-exp ': '') +
             `${deep}:`).padStart(12)+ ' ' + tab(deep); 
 
-    S.DEBUG && 
-        console.log(before(true) + todo); 
+    console.log(before(true) + todo); 
 
     args.forEach(e => {
         if (e instanceof S){
             e.log(deep + 1); 
         } else {
-            S.DEBUG && 
-                console.log(before(false) + e); 
+            console.log(before(false) + e); 
         }
     })
 }
-
-/**
- * @description Alias 
- * @param { String } todo 
- */
-S.$ = list => new S(list); 
