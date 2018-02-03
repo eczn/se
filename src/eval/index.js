73a01c194b5eb4437150689de1bbb62c215796fd
eczn <1975845422@qq.com>
2018-02-04 01:11:00 +0800

    [A] 引入 Scope

--- src/eval/index.js
+++ src/eval/index.js
@@ -1,14 +1,42 @@
+const Scope = require('../Scope')
+    , S = require('../S')
+
 module.exports = eval; 
 
 let calc = {
-    '+': (...args) => args.reduce((acc, cur) => acc + cur), 
-    '-': (...args) => args.reduce((acc, cur) => acc - cur)
+    '+': function(...args){
+        
+        
+        return args.reduce((acc, cur) => acc + cur)
+    }, 
+    '-': (...args) => args.reduce((acc, cur) => acc - cur), 
+    'log': (...args) => console.log.apply(console, args)
 }
 
-function eval(ast){
-    const { list } = ast; 
+/**
+ * 
+ * @param { S } ast 
+ * @param { Scope } scope 
+ */
+function eval(ast_blocks, scope = Scope.global_scope){
 
+    ast_blocks.forEach(ast => {
+        // console.log(ast)
+        evalAst(ast, scope); 
+    }); 
+}
+
+function evalAst(ast, scope){
+    const { list } = ast; 
     let [x, ...xs] = list; 
-    
-    return calc[x].apply(this, xs); 
+
+    if (x === 'define'){
+        let [key, binding] = xs; 
+        
+        scope.define(key, binding); 
+        return; 
+    } else {
+
+        // return calc[x].apply(this, xs); 
+    }
 }
