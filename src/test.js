a88a6f4c6998d2776f26928f246ffa287ee495fb
eczn <1975845422@qq.com>
2018-02-08 17:42:35 +0800

    [M] 用例

--- src/test.js
+++ src/test.js
@@ -8,14 +8,18 @@ const fs = require('then-fs')
 
 let text = fs.readFileSync('./code.se').toString(); 
 
-S.DEBUG = true; 
+// S.DEBUG = true; 
 
 console.time(padStart('parse  ', 11))
 let ast = parse(text); 
 console.timeEnd(padStart('parse  ', 11))
 
+console.log('- SE Log Start ----------------', '\n\n'); 
 console.time(padStart('eval  ', 11))
 let val = eval(ast); 
+
+console.log('\n'); 
+console.log('- SE Log End ------------------'); 
 console.timeEnd(padStart('eval  ', 11))
 
 console.log(padStart('result ', 10), ':', val); 
