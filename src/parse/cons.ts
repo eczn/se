a7cff6260a64e7da679ae3903eba46d2e7589168
ecznlai <ecznlai@tencent.com>
2019-01-16 19:57:03 +0800

    ~ 改成树

--- src/parse/cons.ts
+++ src/parse/cons.ts
@@ -1,25 +1,51 @@
-type Pair = Function;
+type Tree = Function;
 
-function cons(x: number, y: number): Pair {
+function Tree<T = number>(v: T, l?: Tree, r?: Tree): Tree {
     return (how_to_do_it: Function) => {
-        return how_to_do_it(x, y); 
+        return how_to_do_it(v, l, r); 
     }
 }
 
-function car(pair: Pair) {
-    return pair((l, r) => l)
+function lchild(tree: Tree) {
+    return tree((v, l, r) => l);
 }
 
-function cdr(pair: Pair) {
-    return pair((l, r) => r)
+function rchild(tree: Tree) {
+    return tree((v, l, r) => r); 
 }
 
-const xy = cons(1, 2); 
+function valOf(tree: Tree) {
+    return tree((v, l, r) => v); 
+}
 
-const x = car(xy); 
 
-const y = cdr(xy); 
+// 前序遍历
+function tralvel(t?: Tree) {
+    if (!t) return;
 
-console.log(xy); 
-console.log(x); 
-console.log(y); 
+    const l = lchild(t); 
+    const r = rchild(t); 
+    const v = valOf(t); 
+
+    console.log(v); 
+    
+    tralvel(l); 
+    tralvel(r); 
+}
+
+// 创建一棵树。。。 虽然有点绕... 
+const t = 
+               Tree(1,
+    Tree(2, 
+Tree(3), Tree(4)), Tree(5, Tree(6)))
+// 结构如下： 
+//        1
+//     /    \
+//   2       5 
+//  / \     /
+// 3   4   6 
+
+
+// 先序遍历结果
+tralvel(t); 
+// => 打印：123456
