59332dede29fba281b979290774635e0d7ad52a0
eczn <1975845422@qq.com>
2019-12-01 00:56:07 +0800

    main

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@
 
 import { rawParse } from './parse';
 
+import { runAll } from './exp-calc';
+
 import fs from 'fs';
-import { run } from './runtime';
+
 
 const cliArgs = process.argv.slice(2);
 console.log('入参:', cliArgs);
@@ -13,13 +15,12 @@ const [seFile] = cliArgs;
 if (seFile) {
     if (fs.existsSync(seFile)) {
         const code = fs.readFileSync(seFile, 'utf-8');
-        run(rawParse(code))
+        runAll(rawParse(code))
     } else {
         console.log('文件不存在:', seFile);
         process.exit(-1);
     }
 } else {
-    // repl
+    // todo repl
     
 }
-
